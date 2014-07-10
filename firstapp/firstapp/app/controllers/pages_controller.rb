4bdd9f052c3000f4943e5f6ac83ab4e253aa6c1e
robert karson <rob.karson@gmail.com>
2014-07-09 21:48:15 -0400

    first version of app

---
+++ firstapp/firstapp/app/controllers/pages_controller.rb
@@ -0,0 +1,44 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def all
+	end
+
+  def advertising
+  end
+
+def print
+  end
+
+  def tv
+  end
+
+  def interactive
+  end
+
+  def indentity
+  end
+
+  def packaging
+  end
+
+  def books
+  end
+
+  def typography
+  end
+
+
+  def illustration
+  end
+
+  def photography
+  end
+
+  def info
+  end
+
+   def stuff
+  end
+end
